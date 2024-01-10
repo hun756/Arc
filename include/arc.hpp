cb18fbf481ac60f4cbd9777789c9c062e358da43
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2024-01-10 18:32:34 +0300

    Implement simple Arc class template for atomic reference counting
    - Add Arc class template that wraps a pointer and an atomic counter
    - Add constructors, destructor, assignment operator, and dereference operators
    - Add use_count method to return the number of references to the pointer
    - Use std::atomic for thread-safe operations on the counter

--- include/arc.hpp
+++ include/arc.hpp
@@ -1,13 +1,51 @@
 #ifndef ARC_yrpfns
 #define ARC_yrpfns
 
+#include <atomic>
+// #include <memory>
+
 namespace Arc
 {
+    template <typename T>
     class Arc
     {
+    private:
+        T* ptr;
+        std::atomic<unsigned int>* counter;
+
     public:
-        int testFunc();
+        Arc(T* p = nullptr) : ptr(p), counter(new std::atomic<unsigned int>(1)) {}
+
+        ~Arc()
+        {
+            if (ptr && counter->fetch_sub(1) == 1) {
+                delete ptr;
+                delete counter;
+            }
+        }
+
+        Arc(const Arc& other) : ptr(other.ptr), counter(other.counter) { counter->fetch_add(1); }
+
+        Arc& operator=(const Arc& other)
+        {
+            if (this != &other) {
+                if (ptr && counter->fetch_sub(1) == 1) {
+                    delete ptr;
+                    delete counter;
+                }
+                ptr = other.ptr;
+                counter = other.counter;
+                counter->fetch_add(1);
+            }
+            return *this;
+        }
+
+        T& operator*() const { return *ptr; }
+
+        T* operator->() const { return ptr; }
+
+        unsigned int use_count() const { return counter ? counter->load() : 0; }
     };
 } // namespace Arc
 
-#endif
+#endif /* end of namespace: ARC_yrpfns */
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,13 +1,107 @@
 #include "arc.hpp"
 #include <gtest/gtest.h>
 
-TEST(AddTest, PositiveNumbers) {
-    using  Arc::Arc;
-    Arc sh1;
-    EXPECT_EQ(sh1.testFunc(), 999);
+class ArcTest : public ::testing::Test
+{
+protected:
+    void SetUp() override {}
+
+    void TearDown() override {}
+};
+
+TEST_F(ArcTest, Constructor_InitializesWithNonNullptr_IncrementsCounter)
+{
+    using Arc::Arc;
+    auto raw_ptr = new int(5);
+    Arc<int> arc(raw_ptr);
+
+    ASSERT_EQ(*raw_ptr, *arc);
+    ASSERT_EQ(1, arc.use_count());
 }
 
-int main(int argc, char **argv) {
+TEST_F(ArcTest, CopyConstructor_CopiesArc_IncrementsCounter)
+{
+    using Arc::Arc;
+    Arc<int> original(new int(10));
+    Arc<int> copy = original;
+
+    ASSERT_EQ(*original, *copy);
+    ASSERT_EQ(2, original.use_count());
+    ASSERT_EQ(2, copy.use_count());
+}
+
+TEST_F(ArcTest, MoveConstructor_MovesArc_ResetsOldValue) {
+    using Arc::Arc;
+    Arc<int> original(new int(10));
+    Arc<int> moved(std::move(original));
+
+    ASSERT_EQ(10, *moved);
+}
+
+TEST_F(ArcTest, Destructor_ReleaseResource_WhenLastArcIsDestroyed) {
+    using Arc::Arc;
+    auto raw_ptr = new int(20);
+    {
+        Arc<int> arc1(new int(20));
+        {
+            Arc<int> arc2 = arc1;
+            ASSERT_EQ(2, arc1.use_count());
+        }
+        ASSERT_EQ(1, arc1.use_count());
+    }
+    // Check if resource is released properly. Requires manual verification or a custom deleter.
+}
+
+TEST_F(ArcTest, AssignmentOperator_AssignsArc_IncrementsCounter) {
+    using Arc::Arc;
+    Arc<int> arc1(new int(30));
+    Arc<int> arc2(new int(40));
+    arc2 = arc1;
+
+    ASSERT_EQ(*arc1, *arc2);
+    ASSERT_EQ(2, arc1.use_count());
+    ASSERT_EQ(2, arc2.use_count());
+}
+
+TEST_F(ArcTest, SelfAssignment_DoesNotModifyArc) {
+    using Arc::Arc;
+    Arc<int> arc(new int(50));
+    arc = arc;
+
+    ASSERT_EQ(50, *arc);
+    ASSERT_EQ(1, arc.use_count());
+}
+
+TEST_F(ArcTest, OperatorDereference_AccessesObject) {
+    using Arc::Arc;
+    Arc<int> arc(new int(60));
+
+    ASSERT_EQ(60, *arc);
+}
+
+TEST_F(ArcTest, OperatorArrow_AccessesObjectMembers) {
+    using Arc::Arc;
+    struct TestStruct {
+        int value;
+        int getValue() const { return value; }
+    };
+
+    Arc<TestStruct> arc(new TestStruct{70});
+
+    ASSERT_EQ(70, arc->getValue());
+}
+
+TEST_F(ArcTest, UseCount_ReturnsCorrectCount) {
+    using Arc::Arc;
+    Arc<int> arc1(new int(80));
+    Arc<int> arc2 = arc1;
+
+    ASSERT_EQ(2, arc1.use_count());
+    ASSERT_EQ(2, arc2.use_count());
+}
+
+int main(int argc, char** argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
