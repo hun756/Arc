31d4870c17928321425ed30a0fdff80797b96495
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2024-01-10 15:43:41 +0300

    Finalize Project Configuration and Implement Comprehensive Test Suite

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,18 +1,12 @@
 #include "arc.hpp"
 #include <gtest/gtest.h>
 
-// A dummy function to test
-int add(int a, int b) {
-    return a + b;
-}
-
-// Test case for the add function
 TEST(AddTest, PositiveNumbers) {
-    EXPECT_EQ(add(1, 2), 3);
-    EXPECT_EQ(add(10, 20), 30);
+    using  Arc::Arc;
+    Arc sh1;
+    EXPECT_EQ(sh1.testFunc(), 999);
 }
 
-// Main function
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
